198a774a5700a84088d6e3dc0033eae40a788dff
callistus406 <callistus406@gmail.com>
2023-03-13 22:40:48 +0100

    passport configure

---
+++ backend/authentication/passportConfig.js
@@ -0,0 +1,41 @@
+const LocalStrategy = require('passport-local').Strategy;
+const { LoginValidation } = require('../validation/validation');
+const bcrypt = require('bcrypt');
+const passport = require('passport');
+
+const initializePassport = (passport, getUserByEmail, getUserById) => {
+  async function authenticateUser(email, password, done) {
+    const validateData = { email, password };
+    console.log(validateData);
+    const { error } = new LoginValidation(validateData).validate();
+
+    if (error) return done(null, false, { message: error.message });
+
+    const user = await getUserByEmail(email);
+
+    if (user == null) return done(null, false, { message: 'Invalid Credentials' });
+    const fetchUser = user?.dataValues;
+
+    try {
+      if (await bcrypt.compare(password, fetchUser.password)) {
+        // console.log(fetchUser)
+        return done(null, fetchUser);
+      } else {
+        return done(null, false, {
+          message: 'Incorrect Username or Password',
+        });
+      }
+    } catch (e) {
+      return done(e);
+    }
+  }
+
+  passport.use(new LocalStrategy({ usernameField: 'email' }, authenticateUser));
+  passport.serializeUser((user, done) => done(null, user._id));
+  passport.deserializeUser(async (id, done) => {
+    console.log(id);
+    return done(null, await getUserById(id));
+  });
+};
+
+module.exports = initializePassport;
