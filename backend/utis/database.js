9f15ae1df193f49c6f666eba78b4fb73eff72cf7
callistus406 <callistus406@gmail.com>
2023-03-13 22:38:09 +0100

    removed

--- backend/utis/database.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize('bizzapdb', 'newuser1', 'password27$', {
-  dialect: 'mysql',
-  host: 'localhost',
-});
-
-module.exports = sequelize;
