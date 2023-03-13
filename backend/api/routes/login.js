2e6670c871be800d43866c014afbf15016193072
callistus406 <callistus406@gmail.com>
2023-03-13 22:39:32 +0100

    login route

---
+++ backend/api/routes/login.js
@@ -0,0 +1,38 @@
+const router = require('express').Router();
+const passport = require('passport');
+const initializePassport = require('../../authentication/passportConfig');
+const UserModel = require('../../models/userModel');
+
+initializePassport(
+  passport,
+  (email) => UserModel.findOne({ where: { email: email } }),
+  (id) => UserModel.findOne({ where: { _id: id } })
+);
+router.post(
+  '/login',
+
+  (req, res, next) => {
+    passport.authenticate('local', (error, user, info) => {
+      if (error) return res.status(400).json({ success: false, payload: info });
+      if (!user) return res.status(404).json({ success: false, payload: info });
+      if (user) {
+        req.login(user, function (err) {
+          if (err) {
+            return next(err);
+          }
+          return res.redirect('/api/v1/dashboard');
+        });
+      }
+    })(req, res, next);
+  }
+);
+
+router.post('/logout', (req, res, next) => {
+  req.logOut(function (error) {
+    if (error) return next(error);
+    return res.redirect('/');
+  });
+  return res.status(200).json({ success: true, payload: 'you are successfully  logged out' });
+});
+
+module.exports = router;
