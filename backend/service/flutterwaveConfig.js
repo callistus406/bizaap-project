10046c90b77b3f263aa6f56dc48554d3ed5266e5
callistus406 <callistus406@gmail.com>
2023-03-21 14:45:32 +0100

    flutterwave config

---
+++ backend/service/flutterwaveConfig.js
@@ -0,0 +1,5 @@
+require('dotenv').config();
+const Flutterwave = require('flutterwave-node-v3');
+const flw = new Flutterwave(process.env.FLW_PUBLIC_KEY, process.env.FLW_SECRETE_KEY);
+
+module.exports = flw;
