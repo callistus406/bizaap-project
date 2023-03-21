c186966ee34233eb1084cdeaf3fc8090a65c73aa
callistus406 <callistus406@gmail.com>
2023-03-21 14:45:46 +0100

    modified

--- backend/utils/errorHandler.js
+++ backend/utils/errorHandler.js
@@ -1,12 +1,10 @@
 const { CustomError } = require('./customError.js');
-const errorHandler = (error, req, res, next) => { 
+const errorHandler = (error, req, res, next) => {
   if (error instanceof CustomError) {
-    return res
-      .status(error.statusCode)
-      .json({ success: false, payload: error.message });
+    return res.status(error.statusCode).json({ success: false, payload: error.message });
   }
-
-  return res.status(500).send({success:false,payload:error.message});
+  // todo: change this to custom error
+  return res.status(500).send({ success: false, payload: error });
 };
 
-module.exports = errorHandler
+module.exports = errorHandler;
