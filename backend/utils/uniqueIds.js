d1ec2e4a9e99a54ae1943162fb0af0049a5ac704
callistus406 <callistus406@gmail.com>
2023-03-21 14:47:46 +0100

    id gen

--- backend/utils/uniqueIds.js
+++ backend/utils/uniqueIds.js
@@ -1,6 +1,3 @@
-const { v4: uniqueIdv4 } = require('uuid');
-; // ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
-
-
- const generateUniqueId = ()=>uniqueIdv4();
-module.exports = {generateUniqueId}
+const { v4: uniqueIdv4 } = require('uuid'); // ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
+const generateUniqueId = () => uniqueIdv4();
+module.exports = { generateUniqueId };
