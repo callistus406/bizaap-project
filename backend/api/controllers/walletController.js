0da0990903f6ac6910b20f8c312596291a6023cf
callistus406 <callistus406@gmail.com>
2023-03-25 11:55:35 +0100

    added  create wallet pin Controller

--- backend/api/controllers/walletController.js
+++ backend/api/controllers/walletController.js
@@ -2,6 +2,8 @@ const asyncWrapper = require('../../middleware/asyncWrapper');
 const WalletModel = require('../../models/walletModel');
 const generateAccountNumber = require('../../utils/accountNumberGen');
 const { generateUniqueId } = require('../../utils/uniqueIds');
+const { HashPassword } = require('../../authentication/password');
+
 const createWallet = asyncWrapper(async (req, res) => {
   const { walletPin, confirmWalletPin } = req.body;
   const phone = req.user.dataValues.phone;
@@ -40,4 +42,37 @@ const getWallet = asyncWrapper(async (req, res) => {
       .send({ success: false, payload: "Sorry, You don't have a wallet.Please create One " });
   return res.status(200).send({ success: true, wallet });
 });
-module.exports = { createWallet, getWallet };
+
+// create wallet pin
+
+const createWalletPin = asyncWrapper(async (req, res, next) => {
+  const { walletPin, confirmWalletPin } = req.body;
+  const loggedInUser = req.user?.user_id;
+
+  //   validation
+  if (!walletPin || !confirmWalletPin)
+    return res.status(400).send({ success: false, payload: 'Inputs can not be empty' });
+  if (isNaN(walletPin) || isNaN(confirmWalletPin))
+    return res.status(400).send({ success: false, payload: 'Pin cannot be a  string' });
+  if (walletPin.length !== 4 || confirmWalletPin.length !== 4)
+    return res.status(400).send({ success: false, payload: 'Wallet pin must be 4 digits' });
+  if (walletPin !== confirmWalletPin)
+    return res
+      .status(400)
+      .send({ success: false, payload: 'The provided pin do not match, please try again' });
+  const hashedPin = await new HashPassword(walletPin).hash();
+  if (!hashedPin)
+    return next(createCustomError('Sorry!, Something went wrong,please try again later', 400));
+  const createPin = await WalletModel.update(
+    { wallet_pin: hashedPin },
+    { where: { wallet_owner: loggedInUser } }
+  );
+  if (!createPin)
+    return next(createCustomError('Sorry!, Something went wrong,please try again later', 500));
+  if (!createPin[0])
+    return next(
+      createCustomError('Sorry!, System is unable to create pin. please try again later', 500)
+    );
+  return res.status(200).send({ success: true, payload: createPin });
+});
+module.exports = { createWallet, getWallet, createWalletPin };
