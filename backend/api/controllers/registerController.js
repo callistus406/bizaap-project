5c77f3b6fa6d8ae669e3b12806eb3eadae1913c6
callistus406 <callistus406@gmail.com>
2023-03-25 11:55:02 +0100

    added create wallet fn

--- backend/api/controllers/registerController.js
+++ backend/api/controllers/registerController.js
@@ -3,8 +3,11 @@ const { generateUniqueId } = require('../../utils/uniqueIds');
 const UserModel = require('../../models/userModel');
 const { RegisterValidation } = require('../../validation/validation');
 const { HashPassword } = require('../../authentication/password');
+const { createCustomError } = require('../../middleware/customError');
+const generateAccountNumber = require('../../utils/accountNumberGen');
 const flw = require('../../service/flutterwaveConfig');
-const registerController = asyncWrapper(async (req, res) => {
+const WalletModel = require('../../models/walletModel');
+const registerController = asyncWrapper(async (req, res, next) => {
   const { email, businessName, phone, password } = req.body;
   const validateData = { email, businessName, phone, password };
   // payload validation
@@ -15,20 +18,16 @@ const registerController = asyncWrapper(async (req, res) => {
   const isRegistered = await UserModel.findOne({ where: { email: email } });
 
   if (isRegistered)
-    return res
-      .status(302)
-      .send({ success: false, payload: 'Account Already Exist, Please Try Another Email' });
+    return next(createCustomError('Account Already Exist, Please Try Another Email', 400));
   // password hash
   const hashPassword = await new HashPassword(password).hash();
   if (!hashPassword)
-    return res.status(400).json({
-      success: false,
-      payload: 'Sorry!, Something went wrong,please try again later',
-    });
+    return next(createCustomError('Sorry!, Something went wrong,please try again later', 400));
 
   // const isVerified = await flw.Misc.verify_Account(details);
   // User creation
   const uniqueId = generateUniqueId();
+
   const createdUser = await UserModel.create({
     _id: uniqueId,
     email,
@@ -38,8 +37,16 @@ const registerController = asyncWrapper(async (req, res) => {
     password: hashPassword,
     // phone,/
   });
+
+  // create wallet
+  const walletCode = generateAccountNumber(createdUser.phone);
+  const createWallet = await WalletModel.create({
+    wallet_owner: createdUser.user_id,
+    wallet_code: walletCode,
+  });
+
   // response
-  return res.status(201).json({ success: true, payload: createdUser });
+  return res.status(201).json({ success: true, payload: createdUser, createWallet });
 });
 
 module.exports = {
