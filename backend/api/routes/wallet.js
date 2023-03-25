e78329072687510ab954e8ebedbbbcd78b092501
callistus406 <callistus406@gmail.com>
2023-03-25 11:58:24 +0100

    create wallet pin route

--- backend/api/routes/wallet.js
+++ backend/api/routes/wallet.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
-const { createWallet, getWallet } = require('./../controllers');
+const { createWallet, getWallet, createWalletPin } = require('./../controllers');
 
 router.post('/customer/create/wallet', createWallet);
 router.get('/customer/find/wallet', getWallet);
+router.patch('/customer/wallet/create_pin', createWalletPin);
 
 module.exports = router;
