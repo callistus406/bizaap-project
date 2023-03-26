708d86fdffb936866489f89825498a7e8c16d536
callistus406 <callistus406@gmail.com>
2023-03-26 08:57:17 +0100

    added session store

--- backend/app.js
+++ backend/app.js
@@ -6,23 +6,42 @@ const routes = require('./api');
 const { connectDb } = require('./db/connect');
 const session = require('express-session');
 const passport = require('passport');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const { sequelize } = require('./db/connect');
 const PORT = process.env.PORT || 4000;
 const errorHandler = require('./middleware/errorHandler');
 // require('./service/flutterwaveConfig');
+
+// store config
+
+const sessionStore = new SequelizeStore({
+  db: sequelize,
+  expiration: 60 * 60 * 60 * 1000, // 1 hr
+  checkExpirationInterval: 15 * 60 * 1000, // 15 minutes
+  cookie: {
+    maxAge: 60 * 60 * 1000, // 1 hr
+    httpOnly: true,
+    secure: true,
+  },
+});
+
 // middle wares
+app.use(cookiePasser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   session({
     secret: process.env.SESSION_SECRETE,
-    resave: true,
-    saveUninitialized: true,
+    resave: false,
+    saveUninitialized: false,
+    store: sessionStore,
   })
 );
 app.use(passport.initialize());
 app.use(passport.session());
-
+// application route
 app.use('/api/v1', routes);
+// error handler middleware
 app.use(errorHandler);
 const startApp = async () => {
   try {
