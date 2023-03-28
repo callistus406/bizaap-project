bdee3660a6e7fcf31bf812ff68e537fe6be0f76b
callistus406 <callistus406@gmail.com>
2023-03-28 12:31:59 +0100

    new update

--- backend/api/routes/login.js
+++ backend/api/routes/login.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const passport = require('passport');
 const initializePassport = require('../../authentication/passportConfig');
 const UserModel = require('../../models/userModel');
-
+const GatewayModel = require('../../models/gatewayModel');
 initializePassport(
   passport,
   (email) => UserModel.findOne({ where: { email: email } }),
@@ -16,10 +16,20 @@ router.post(
       if (error) return res.status(400).json({ success: false, payload: info });
       if (!user) return res.status(401).json({ success: false, payload: info });
       if (user) {
+        // console.log(req.logIn);
         req.login(user, function (err) {
+          console.log('err');
           if (err) {
             return next(err);
           }
+          // (async function () {
+          //   await GatewayModel.create({
+          //     gateway_name: 'flutterwave',
+          //     gateway_type: 'payment',
+          //     status: 'active',
+          //   });
+          // })();
+
           return res.redirect('/api/v1/dashboard');
         });
       }
