3c327b6ce8496e1bc10fe4d681fbc98c84edb2fe
callistus406 <callistus406@gmail.com>
2023-03-30 12:56:25 +0100

    reset password Fn

--- backend/api/controllers/registerController.js
+++ backend/api/controllers/registerController.js
@@ -1,22 +1,47 @@
 const asyncWrapper = require('../../middleware/asyncWrapper');
 const { generateUniqueId } = require('../../utils/uniqueIds');
-const UserModel = require('../../models/userModel');
 const { RegisterValidation } = require('../../validation/validation');
 const { HashPassword } = require('../../authentication/password');
 const { createCustomError } = require('../../middleware/customError');
 const generateAccountNumber = require('../../utils/accountNumberGen');
 const flw = require('../../service/flutterwaveConfig');
 const WalletModel = require('../../models/walletModel');
+const UserModel = require('../../models/userModel');
+const { sendMailOTP } = require('../../service/sendOTP');
+const { Op } = require('sequelize');
 const registerController = asyncWrapper(async (req, res, next) => {
-  const { email, businessName, phone, password } = req.body;
-  const validateData = { email, businessName, phone, password };
+  const { email, fullName, username, bvn, phone, password, confirmPassword } = req.body;
+  const validateData = { email, fullName, username, phone, password, bvn };
   // payload validation
   const { error } = new RegisterValidation(validateData).checkValidation();
 
   if (error) return res.status(200).json({ success: false, payload: error.message });
+  console.log(password, confirmPassword);
+  if (password !== confirmPassword)
+    return next(createCustomError('The passwords entered do not match. ', 400));
+  const findUserName = await UserModel.findOne({
+    where: {
+      username: {
+        [Op.regexp]: `(${username}|${username.toUpperCase()})`,
+      },
+    },
+  });
+  const full_name = fullName;
+  if (findUserName)
+    return next(
+      createCustomError('Sorry You cannot use this username.please try another one', 400)
+    );
   // user lookup
-  const isRegistered = await UserModel.findOne({ where: { email: email } });
+  const isPhoneFound = await UserModel.findOne({ where: { phone: phone } });
+  if (isPhoneFound)
+    return next(
+      createCustomError(
+        'Sorry, Phone number already exist in our system, please try another one',
+        400
+      )
+    );
 
+  const isRegistered = await UserModel.findOne({ where: { email: email } });
   if (isRegistered)
     return next(createCustomError('Account Already Exist, Please Try Another Email', 400));
   // password hash
@@ -25,28 +50,25 @@ const registerController = asyncWrapper(async (req, res, next) => {
     return next(createCustomError('Sorry!, Something went wrong,please try again later', 400));
 
   // const isVerified = await flw.Misc.verify_Account(details);
-  // User creation
-  const uniqueId = generateUniqueId();
+  // const redirectLink = "/"
+  // TODO: verify BVN
 
-  const createdUser = await UserModel.create({
-    _id: uniqueId,
-    email,
-    businessName,
-    phone,
-    // bankAccount,
-    password: hashPassword,
-    // phone,/
-  });
+  // TODO: verify OTP
+  req.session.customer_details = {};
+  req.session.customer_details.password = hashPassword;
+  req.session.customer_details.email = email;
+  req.session.customer_details.phone = phone;
+  req.session.customer_details.username = username;
+  req.session.customer_details.bvn = bvn;
+  req.session.customer_details.full_name = full_name;
 
-  // create wallet
-  const walletCode = generateAccountNumber(createdUser.phone);
-  const createWallet = await WalletModel.create({
-    wallet_owner: createdUser.user_id,
-    wallet_code: walletCode,
+  sendMailOTP(email, 'redirectLink', req);
+  return res.status(200).json({
+    success: true,
+    payload: { message: `OTP has been sent to ${email}`, redirectUrl: '/customer/validate_otp' },
   });
 
   // response
-  return res.status(201).json({ success: true, payload: createdUser, createWallet });
 });
 
 module.exports = {
---
+++ backend/api/controllers/resetPasswordController.js
@@ -0,0 +1,76 @@
+const jwt = require('jsonwebtoken');
+const UserModel = require('../../models/userModel');
+const { sendResetEmail } = require('../../service/sendMail');
+const asyncWrapper = require('../../middleware/asyncWrapper');
+const { HashPassword } = require('../../authentication/password');
+const { createCustomError } = require('../../middleware/customError');
+const ResetPasswordModel = require('../../models/resetPasswordModel');
+const { EmailValidation, PasswordValidation } = require('../../validation/validation');
+const resetPassword = asyncWrapper(async (req, res, next) => {
+  const email = req.body.email;
+  const { error } = new EmailValidation({ email: email }).validate();
+  if (error) return next(createCustomError(error.message, 400));
+  // generate a unique token for this reset request
+  const resetToken = jwt.sign({ email }, process.env.RESET_TOKEN_SECRET, { expiresIn: '1h' });
+
+  // store the reset token in your database, along with the user's email address or username
+  const isTrue = await storeResetToken(email, resetToken);
+
+  // send an email to the user with a link that includes the reset token
+  const resetUrl = `customer/reset_password/${resetToken}`;
+  // send email to the customer
+  const message = await sendResetEmail(email, resetUrl);
+  // return a response indicating that the reset request was successful
+  res.status(200).json({ success: true, payload: 'Password reset email sent' });
+});
+
+const confirmResetPassword = asyncWrapper(async (req, res, next) => {
+  // get info from user from FE
+  const { token } = req.params;
+  const { password } = req.body;
+  const { error } = new PasswordValidation({ password: password }).validate();
+  if (error) return next(createCustomError(error.message, 400));
+  // verify the reset token and retrieve the user's email address or username
+  const { email } = jwt.verify(token, process.env.RESET_TOKEN_SECRET);
+
+  // update the user's password in your database
+  await updatePassword(email, password, next);
+
+  // delete the reset token from your database
+  await deleteResetToken(email, token);
+
+  // return a response indicating that the password reset was successful
+  return res.status(200).json({ success: true, payload: 'Password reset successfully' });
+});
+
+async function storeResetToken(email, token) {
+  // store the reset token in your database, along with the user's email address or username
+  const createRecord = await ResetPasswordModel.create({
+    email: email,
+    token: token,
+  });
+  return createRecord;
+}
+
+async function updatePassword(email, password, next) {
+  // hash the new password using a library like `bcrypt`
+
+  // return;
+  const hashPassword = await new HashPassword(password).hash();
+
+  if (!hashPassword)
+    return next(createCustomError('Sorry!, Something went wrong,please try again later', 400));
+  // update the user's password in your database
+  const updateUser = await UserModel.update(
+    { password: hashPassword },
+    { where: { email: email } }
+  );
+  return updateUser;
+}
+
+async function deleteResetToken(email, token) {
+  const isDeleted = await ResetPasswordModel.destroy({ where: { email: email } });
+
+  return isDeleted;
+}
+module.exports = { resetPassword, confirmResetPassword };
