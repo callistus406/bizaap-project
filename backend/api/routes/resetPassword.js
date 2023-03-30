3025d052ab2188d6d345c71aaacce10b89e13d66
callistus406 <callistus406@gmail.com>
2023-03-30 12:58:16 +0100

    reset password route

---
+++ backend/api/routes/resetPassword.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const VerifyUser = require('../../middleware/auth');
+const { resetPassword, confirmResetPassword } = require('../controllers');
+
+router.post('/customer/reset_password', resetPassword);
+router.post('/customer/confirm_password/:token', confirmResetPassword);
+
+module.exports = router;
