2d716bd411fa95cf0b9e52216c10580cacb64574
callistus406 <callistus406@gmail.com>
2023-04-06 10:06:26 +0100

    new update

--- backend/utils/transactionLogger.js
+++ backend/utils/transactionLogger.js
@@ -1,22 +1,26 @@
 const TransactionLogModel = require('../models/transactionLogModel');
 const DepositModel = require('../models/depositModel');
 const TransferModel = require('../models/transferModel');
+const BillsModel = require('../models/billsModel');
 
 const transactionLogger = async ({ type, amount, customer_id, tx_ref, status }) => {
   let transaction_id;
   if (type === 'Deposit') {
     const getDepositInfo = await DepositModel.findOne({ where: { tx_ref_code: tx_ref } });
     transaction_id = getDepositInfo.deposit_id;
-    console.log('qwerty', getDepositInfo);
+    // console.log('qwerty', getDepositInfo);
   } else if (type === 'Transfer') {
     const getTransfer = await TransferModel.findOne({ where: { transaction_ref: tx_ref } });
-    console.log('----------rrrrrr----------');
-    console.log(getTransfer);
+    // console.log('----------rrrrrr----------');
+    // console.log(getTransfer);
     transaction_id = getTransfer.dataValues?.transfer_id;
   } else if (type === 'Withdrawal') {
     const getTransfer = await TransferModel.findOne({ where: { transaction_ref: tx_ref } });
 
     transaction_id = getTransfer.transaction_code;
+  } else if (type === 'Bills') {
+    const getTransfer = await BillsModel.findOne({ where: { tx_ref: tx_ref } });
+    transaction_id = getTransfer.bills_id;
   } else {
     console.log('in progress');
   }
