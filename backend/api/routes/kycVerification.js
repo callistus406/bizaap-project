3fd3759b85753c2677f2d1c4f80c25a4a954b5f5
callistus406 <callistus406@gmail.com>
2023-04-08 00:24:58 -0700

    quick modification

--- backend/api/routes/kycVerification.js
+++ backend/api/routes/kycVerification.js
@@ -1,11 +1,20 @@
 const router = require('express').Router();
+const asyncWrapper = require('../../middleware/asyncWrapper');
 const VerifyUser = require('../../middleware/auth');
+const KycModel = require('../../models/kycModel');
 const { initiateMediaTransfer } = require('../../service/multerConfig');
 const {
   checkKycVerification,
   kycVerificationCtrl,
   kycVerificationFinalStageCtrl,
 } = require('../controllers');
+
+// const authorizeTransaction = asyncWrapper(async (req, res, next) => {
+//   const { user_id } = req.user;
+//   const getUserinfo = await KycModel.findOne({ where: { user_id: 3 } });
+
+//   res.status(200).send({ success: true, payload: getUserinfo });
+// });
 
 router.get('/customer/check/kyc', VerifyUser.ensureAuthenticated, checkKycVerification);
 router.post(
@@ -22,4 +31,5 @@ router.post(
   kycVerificationFinalStageCtrl
 );
 
+// router.get('/customer/verify', authorizeTransaction);
 module.exports = router;
