b31bd84aa846165da62ef07dd7ea81fea39d141f
callistus406 <callistus406@gmail.com>
2023-04-10 02:48:03 -0700

    refectored

--- backend/utils/transactionLogger.js
+++ backend/utils/transactionLogger.js
@@ -3,33 +3,40 @@ const DepositModel = require('../models/depositModel');
 const TransferModel = require('../models/transferModel');
 const BillsModel = require('../models/billsModel');
 
-const transactionLogger = async ({ type, amount, customer_id, tx_ref, status }) => {
-  let transaction_id;
-  if (type === 'Deposit') {
-    const getDepositInfo = await DepositModel.findOne({ where: { tx_ref_code: tx_ref } });
-    transaction_id = getDepositInfo.deposit_id;
-    // console.log('qwerty', getDepositInfo);
-  } else if (type === 'Transfer') {
-    const getTransfer = await TransferModel.findOne({ where: { transaction_ref: tx_ref } });
-    // console.log('----------rrrrrr----------');
-    // console.log(getTransfer);
-    transaction_id = getTransfer.dataValues?.transfer_id;
-  } else if (type === 'Withdrawal') {
-    const getTransfer = await TransferModel.findOne({ where: { transaction_ref: tx_ref } });
+const transactionLogger = async ({
+  transaction_type,
+  amount,
+  account_number,
+  description,
+  tx_ref,
+  status,
+  customer_id,
+}) => {
+  // let transaction_id;
+  // if (transaction_type === 'Credit') {
+  //   const getDepositInfo = await DepositModel.findOne({ where: { tx_ref_code: tx_ref } });
+  //   transaction_id = getDepositInfo.deposit_id;
+  // } else if (transaction_type === 'dsdsd') {//todo
+  //   const getTransfer = await TransferModel.findOne({ where: { transaction_ref: tx_ref } });
+  //   transaction_id = getTransfer.dataValues?.transfer_id;
+  // } else if (transaction_type === 'Withdrawal') {
+  //   const getTransfer = await TransferModel.findOne({ where: { transaction_ref: tx_ref } });
+  //   transaction_id = getTransfer.transaction_code;
+  // } else if (transaction_type === 'Bills') {
+  //   const getTransfer = await BillsModel.findOne({ where: { tx_ref: tx_ref } });
+  //   transaction_id = getTransfer.bills_id;
+  // } else {
+  //   console.log('in progress');
+  // }
 
-    transaction_id = getTransfer.transaction_code;
-  } else if (type === 'Bills') {
-    const getTransfer = await BillsModel.findOne({ where: { tx_ref: tx_ref } });
-    transaction_id = getTransfer.bills_id;
-  } else {
-    console.log('in progress');
-  }
   const updateTxLog = await TransactionLogModel.create({
-    type,
+    transaction_type,
     amount,
-    transaction_id,
-    user_id: customer_id,
+    account_number,
+    tx_ref,
+    description,
     status,
+    customer_id,
   });
 
   return updateTxLog;
