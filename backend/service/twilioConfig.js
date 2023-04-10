658a57e63f1b0dcd8d2be85691969f791be6760f
callistus406 <callistus406@gmail.com>
2023-04-10 02:50:49 -0700

    twilio config

---
+++ backend/service/twilioConfig.js
@@ -0,0 +1,18 @@
+const asyncWrapper = require('../middleware/asyncWrapper');
+require('dotenv').config();
+const variable = process.env;
+const accountSid = variable.TWILIO_ACCT_SID;
+const authToken = variable.TWILIO_ACCT_SECRETE;
+const client = require('twilio')(accountSid, authToken);
+
+const sendSMS = asyncWrapper(async (fromNumber, toNumber, message) => {
+  const response = await client.messages.create({
+    from: fromNumber,
+    to: toNumber,
+    body: message,
+  });
+
+  return response;
+});
+
+module.exports = { sendSMS };
