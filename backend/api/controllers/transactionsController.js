9e2885ccec0067c808f2fb5cf24e860df00b79e0
callistus406 <callistus406@gmail.com>
2023-04-13 06:53:01 -0700

    HOT FIX

--- backend/api/controllers/transactionsController.js
+++ backend/api/controllers/transactionsController.js
@@ -3,32 +3,40 @@ const TransferModel = require('../../models/transferModel');
 const DepositModel = require('../../models/depositModel');
 const UserModel = require('../../models/userModel');
 const { sequelize } = require('../../db/connect');
-const expense = asyncWrapper(async (req, res) => {
+const WalletModel = require('../../models/walletModel');
+const { createCustomError } = require('../../middleware/customError');
+const expense = asyncWrapper(async (req, res, next) => {
   const loggedInUser = req.user?.user_id;
-
+  const customerWallet = await WalletModel.findOne({ where: { wallet_owner: loggedInUser } });
+  if (!customerWallet)
+    return next(createCustomError('Sorry,something went wrong.please try again later', 500));
   const getExpense = await TransferModel.findAll({
     attributes: ['amount', 'updatedAt', 'charged', 'remark'],
     where: {
-      account_owner: loggedInUser,
+      sender_account: customerWallet.dataValues.wallet_code,
     },
   });
 
-  if (!getExpense) return res.status(404).send({ success: false, payload: 'No record found' });
+  if (getExpense.length == 0)
+    return res.status(404).send({ success: false, payload: 'No record found' });
 
   res.status(200).send({ success: true, payload: getExpense });
 });
 
 const income = asyncWrapper(async (req, res) => {
   const loggedInUser = req.user?.user_id;
-
+  const customerWallet = await WalletModel.findOne({ where: { wallet_owner: loggedInUser } });
+  if (!customerWallet)
+    return next(createCustomError('Sorry,something went wrong.please try again later', 500));
   const income = await DepositModel.findAll({
     attributes: ['amount', 'updatedAt', 'remark'],
     where: {
-      receiver: loggedInUser,
+      account_number: customerWallet.dataValues.wallet_code,
     },
   });
 
-  if (!income) return res.status(404).send({ success: false, payload: 'No record found' });
+  if (income.length == 0)
+    return res.status(404).send({ success: false, payload: 'No record found' });
 
   res.status(200).send({ success: true, payload: income });
 });
@@ -36,9 +44,26 @@ const income = asyncWrapper(async (req, res) => {
 const allTransactions = asyncWrapper(async (req, res) => {
   const loggedInUser = req.user?.user_id;
 
-  const response = await sequelize.query(
-    'SELECT deposits.amount AS income_amount,deposits.remark AS income_desc ,deposits.updatedAt AS income_date,transfers.amount AS expense_amount,transfers.updatedAt AS expense_date,transfers.charged AS expense_charge, transfers.remark AS  expense_desc  FROM deposits LEFT JOIN transfers ON deposits.deposit_id = transfers.transfer_id '
-  );
+  // TODO:CONSIDER JOINING THE EXPENSE AND INCOME TABLE IF FEASIBLE
+  // const response = await sequelize.query(
+  //   'SELECT deposits.amount AS income_amount,deposits.remark AS income_desc ,deposits.updatedAt AS income_date,transfers.amount AS expense_amount,transfers.updatedAt AS expense_date,transfers.charged AS expense_charge, transfers.remark AS  expense_desc  FROM deposits LEFT JOIN transfers ON deposits.deposit_id = transfers.transfer_id '
+  // );
+  const customerWallet = await WalletModel.findOne({ where: { wallet_owner: loggedInUser } });
+  if (!customerWallet)
+    return next(createCustomError('Sorry,something went wrong.please try again later', 500));
+  const income = await DepositModel.findAll({
+    attributes: ['amount', 'updatedAt', 'remark'],
+    where: {
+      account_number: customerWallet.dataValues.wallet_code,
+    },
+  });
+  const getExpense = await TransferModel.findAll({
+    attributes: ['amount', 'updatedAt', 'charged', 'remark'],
+    where: {
+      sender_account: customerWallet.dataValues.wallet_code,
+    },
+  });
+  const response = { income: [...income], expense: [...getExpense] };
 
   if (!response) return res.status(404).send({ success: false, payload: 'No record found' });
 
@@ -46,6 +71,3 @@ const allTransactions = asyncWrapper(async (req, res) => {
 });
 
 module.exports = { expense, income, allTransactions };
-
-// select deposits.amount AS income_amount,deposits.remark as income_desc ,deposits.updatedAt as income_date,transfers.amount AS expense_amount,transfers.updatedAt AS expense_date,transfers.charged AS expense_charge, transfers.remark AS  expense_desc  FROM deposits LEFT JOIN transfers ON deposits.receiver = transfers.account_owner ;
-// SELECT deposits.amount AS income_amount,deposits.remark as income_desc ,deposits.updatedAt as income_date,transfers.amount AS expense_amount,transfers.updatedAt AS expense_date,transfers.charged AS expense_charge, transfers.remark AS  expense_desc  FROM deposits LEFT JOIN transfers ON deposits.deposit_id = transfers.transfer_id ;
