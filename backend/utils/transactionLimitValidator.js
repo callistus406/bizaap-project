12fbbc0d300a4b1d057b00a387b053be5dda82ff
callistus406 <callistus406@gmail.com>
2023-04-13 03:54:06 -0700

    extracted date formatter

--- backend/utils/transactionLimitValidator.js
+++ backend/utils/transactionLimitValidator.js
@@ -1,17 +1,11 @@
 const { Op } = require('sequelize');
 const TransactionLogModel = require('../models/transactionLogModel');
 const { sequelize } = require('../db/connect');
-const formatDate = (date) => {
-  let day = String(date.getDate()).padStart(2, '0');
-  let month = String(date.getMonth() + 1).padStart(2, '0');
-  let year = String(date.getFullYear());
-  return `${year}-${month}-${day}`;
-};
+const { formatDate } = require('./formatters');
 
 const validateTransactionLimit = async ({ _user, _amount }) => {
   const envVar = process.env;
   const currentDate = new Date();
-
   const today = formatDate(currentDate);
   const totalTx = await TransactionLogModel.sum('amount', {
     where: {
@@ -21,9 +15,10 @@ const validateTransactionLimit = async ({ _user, _amount }) => {
     },
   });
   //
+
   //   return;
   let _txLimit = _user.business_name ? envVar.BUSINESS_TX_LIMIT : envVar.INDIVIDUAL_TX_LIMIT;
-  if (!totalTx) return true;
+  if (!totalTx) return { isEligible: true, txLimit: _txLimit, totalTx };
   const totalTxPlusAmt = totalTx + _amount;
   console.log(totalTxPlusAmt);
   const isEligible =
@@ -31,6 +26,7 @@ const validateTransactionLimit = async ({ _user, _amount }) => {
     (!_user.business_name && totalTxPlusAmt > _txLimit)
       ? false
       : true;
+
   return { isEligible, txLimit: _txLimit, totalTx };
 };
 
