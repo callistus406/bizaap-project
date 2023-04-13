1656053f62e2224a217973806d2cb5d5696f9d5e
callistus406 <callistus406@gmail.com>
2023-04-13 06:06:30 -0700

    added some features

--- backend/db/connect.js
+++ backend/db/connect.js
@@ -1,5 +1,7 @@
 const { Sequelize } = require('sequelize');
+const mysql2 = require('mysql2/promise');
 require('dotenv').config();
+
 // Create a new Sequelize instance
 const sequelize = new Sequelize(
   process.env.DB_NAME,
@@ -8,21 +10,31 @@ const sequelize = new Sequelize(
   {
     host: process.env.DB_HOST,
     dialect: 'mysql',
-    logging: false, // Disable logging
-    // dialect: 'mysql' | 'sqlite' | 'postgres' | 'mssql',
-    // pool: createPool({
-    //   max: 10,
-    //   min: 0,
-    //   acquire: 30000,
-    //   idle: 10000,
-    // }),
+    logging: false,
+    pool: {
+      max: 10,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+    dialectOptions: {
+      // ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: true } : false,
+    },
   }
 );
 
 // Test the database connection
 const connectDb = async () => {
   try {
-    await sequelize.authenticate();
+    const connection = await mysql2.createConnection({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      // ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: true } : false,
+    });
+    await connection.connect();
+    await connection.end();
     console.log('Connection to the database has been established successfully.');
   } catch (error) {
     console.error('Unable to connect to the database:', error);
