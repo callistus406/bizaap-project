ccf078f5ec03c9fe40bfcbfad7539e694b466bb0
callistus406 <callistus406@gmail.com>
2023-04-13 06:02:26 -0700

    added commision value

--- backend/utils/txMessageTemplate.js
+++ backend/utils/txMessageTemplate.js
@@ -1,5 +1,5 @@
-const txSMSTemplate = (txType, account, amount, date, description, balance) => {
-  return `${txType}:${account} Amt:${amount} Date:${date},Desc:${description},Bal:${balance}CR`;
+const txSMSTemplate = (txType, account, amount, total_charge, date, description, balance) => {
+  return `${txType}:${account} Amt:${amount} commission:${total_charge} Date:${date},Desc:${description},Bal:${balance}CR`;
 };
 
 module.exports = { txSMSTemplate };
